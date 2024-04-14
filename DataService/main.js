c6acfc3bf0d58ed990d7245ae879d322fefa4d30
Armaani <armaan_preet.ghotra@smail.th-koeln.de>
2024-04-14 23:23:52 +0200

    Data service up n running, JSON mapped completly run Index.html to see what it does (Web-console)

--- DataService/main.js
+++ DataService/main.js
@@ -1,11 +1,10 @@
 class Evaluation {
-    constructor(evaluation, rangeOptions = []) {
+    constructor(evaluation) {
         this.evaluation = evaluation;
-        this.rangeOptions = rangeOptions;
+        
     }
 }
 
-// Define the Indicator class
 class Indicator {
     constructor(indicator, comment, evaluation) {
         this.indicator = indicator;
@@ -14,14 +13,86 @@ class Indicator {
     }
 }
 
-// Define the Sector class
-class Sector {
+class Subsector {
     constructor(name, indicators) {
         this.name = name;
-        this.indicators = indicators.map(indicator => new Indicator(
-            indicator.indicator,
-            indicator.comment,
-            indicator.evaluation,
-        ));
+        this.indicators = indicators;
     }
 }
+
+class Sector {
+    constructor(name, subsectors) {
+        this.name = name;
+        this.subsectors = subsectors;
+    }
+}
+
+// Fetching the JSON data
+fetch('../data/data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        var sectorLib = [];
+        const sectors = Object.keys(data.sectors);
+
+        const subsectorsAndIndicators = {};
+
+        var currentSectors = [];
+        sectors.forEach(sector => {
+            var sectorName = sector; //NAME
+            console.log(sectorName)
+
+            // Get subsectors for each sector
+            const subsectors = Object.keys(data.sectors[sector]); //SUBSECTOR NAMES
+            
+            console.log(subsectors)
+            
+            
+            
+            // Iterate through subsectors
+            var currentSubsectors = [];
+            subsectors.forEach(subsector => {
+                // Get indicators for each subsector
+                console.log("SUBSEC "+subsector)
+                
+
+                const indicators = data.sectors[sector][subsector]; //indicators
+                console.log(indicators);
+
+
+                var currentIndicators = [];
+                indicators.forEach(indicator =>{
+                    
+                    const indicatorName = indicator.indicator;
+                    const indicatorComment = indicator.comment;
+                    const indicatorEvaluation = indicator.evaluation;
+
+                    currentIndicators.push(new Indicator(indicatorName, indicatorComment, indicatorEvaluation));
+
+                    // You can then do whatever you need with these properties
+                    console.log('Indicator Name:', indicatorName);
+                    console.log('Indicator Comment:', indicatorComment);
+                    console.log('Indicator Evaluation:', indicatorEvaluation);
+                    console.log("-----------")
+                    
+                });
+                
+                currentSubsectors.push(new Subsector(subsector, currentIndicators));
+
+                
+            });
+                currentSectors.push(new Sector(sectorName, currentSubsectors));
+
+        });
+
+        console.log("Sectors:", currentSectors);
+        //console.log("Subsectors and Indicators:", subsectorsAndIndicators);
+
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
