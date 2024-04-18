65f5740059413a5542b23c02d9a4c41fb2518e90
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2024-04-18 20:35:58 +0200

    dataloader returnes questionnaire

--- app/src/utils/dataLoader.ts
+++ app/src/utils/dataLoader.ts
@@ -5,22 +5,19 @@ import {Indicator} from "../models/indicator";
 import {Subsector} from "../models/subsector";
 import {Sector} from "../models/sector";
 
-function initializeQuestionnaire(jsonData: any): Questionnaire {
+export function initializeQuestionnaire(jsonData: any): Questionnaire {
     const sectors = Object.keys(jsonData.sectors).map(sectorName => {
         const subsectors = Object.keys(jsonData.sectors[sectorName]).map(subsectorName => {
             const subsectorData = jsonData.sectors[sectorName][subsectorName];
             const indicators = subsectorData.indicators.map((indicator: any) => {
-                // Directly using the JSON structure as the evaluation criteria
                 return new Indicator(indicator.text, indicator.comment, indicator.evaluation);
             });
             return new Subsector(subsectorName, indicators);
         });
         return new Sector(sectorName, subsectors);
     });
-    return new Questionnaire(sectors);
-}
 
-document.addEventListener('DOMContentLoaded', () => {
-    const questionnaire = initializeQuestionnaire(data);
-    // Render the questionnaire or do other setup here
-});
+    const questionnaire = new Questionnaire(sectors);
+    console.log(questionnaire);
+    return questionnaire;
+}
