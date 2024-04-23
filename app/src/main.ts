67478921af97805f7a6e612f726075f3c031fbbc
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2024-04-23 22:16:10 +0200

    question container responsiveness WIP

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,7 @@
 import {initializeQuestionnaire} from "./utils/dataLoader"; // Adjust path as necessary
 import * as data from './data/data.json';
 import {EventManager} from "./services/eventManager";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     const questionnaire = initializeQuestionnaire(data);
