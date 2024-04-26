16290e6dd7d5d08e2199f2a408855f0208acc314
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2024-04-26 02:44:25 +0200

    primitive bar chart

--- app/src/models/questionnaire.ts
+++ app/src/models/questionnaire.ts
@@ -59,24 +59,34 @@ export class Questionnaire {
 
         let opportunities = evaluations.filter(evaluation => evaluation).length;
         let risks = evaluations.length - opportunities;
+        let remaining = evaluations.length - (opportunities + risks); // Calculate remaining indicators
 
         let sectorNameElement = sectorName ? `<h1>${sectorName}</h1>` : '';
         let subsectorNameElement = subsectorName ? `<h2>${subsectorName}</h2>` : '';
         const score = opportunities === 0 && risks === 0 ? 0 : risks ? (opportunities / risks) / (risks + opportunities) : 1;
 
+        // Calculate percentages
+        const oppPercent = (opportunities / evaluations.length) * 100;
+        const riskPercent = (risks / evaluations.length) * 100;
+        const remainingPercent = (remaining / evaluations.length) * 100;
 
         return `
-            <h1 id="result">YOUR RESULT FOR </h1>
-            <span class="sector-Name-result"> ${sectorNameElement} </span>
-            <span class="subsector-Name-reslut"> ${subsectorNameElement}  </span>
-            <p id="indicators">Indicators: ${evaluations.length}</p>
-            <p id="opportunities">Opportunities: ${opportunities}</p>
-            <p id="risk">Risks: ${risks}</p>
-            <p id="score">Score: ${(score * 100).toFixed(0)}%</p>
-            <a href="index.html" class="back-to-home">Zurück zur Startseite</a>
-
-        `;
+        <h1 id="result">YOUR RESULT FOR </h1>
+        <span class="sector-name-result">${sectorNameElement}</span>
+        <span class="subsector-name-result">${subsectorNameElement}</span>
+        <div class="chart">
+            <div class="bar opp" style="width: ${oppPercent}%;">${opportunities}</div>
+            <div class="bar risk" style="width: ${riskPercent}%;">${risks}</div>
+            <div class="bar remaining" style="width: ${remainingPercent}%;">${remaining}</div>
+        </div>
+        <p id="indicators">Indicators: ${evaluations.length}</p>
+        <p id="opportunities">Opportunities: ${opportunities}</p>
+        <p id="risk">Risks: ${risks}</p>
+        <p id="score">Score: ${(score * 100).toFixed(0)}%</p>
+        <a href="index.html" class="back-to-home">Zurück zur Startseite</a>
+    `;
     }
+
 
 
     private renderIncompleteQuestionnaire(): string {
