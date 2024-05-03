5305a9298e6769b26c2bc8407d6978c7c5235b4d
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2024-05-04 01:16:21 +0200

    refactored animation for indicators

--- app/src/models/renderers/subsectorRenderer.ts
+++ app/src/models/renderers/subsectorRenderer.ts
@@ -14,58 +14,56 @@ export class SubsectorRenderer extends Renderer {
         if (!form) throw new Error('Form not found');
 
         const handleNextSubmit = (event: Event) => {
-            const form = event.target as HTMLFormElement;
-            const formData = new FormData(form);
+            event.preventDefault();
+            Renderer.playAnimationOnElementWithId('indicatorContainer', 'fade-out')
+                .then(() => {
+                    const form = event.target as HTMLFormElement;
+                    const formData = new FormData(form);
 
-            const firstChild = form.querySelector('input') as HTMLInputElement;
-            switch (firstChild.name) {
-                case 'multiCheckbox' : {
-                    const checkboxData = formData.get('multiCheckbox');
-                    let booleanData = checkboxData === 'yes' ? true : checkboxData === 'no' ? false : null;
-                    Renderer.questionnaire.storeResponse(booleanData);
-                    break;
-                }
-                case 'singleCheckbox': {
-                    const checkboxData = formData.get('singleCheckbox');
-                    let booleanData = checkboxData === 'yes' ? true : checkboxData === 'no' ? false : null;
-                    Renderer.questionnaire.storeResponse(booleanData);
-                    break;
-                }
-                case 'rangeInput': {
-                    const rangeData = formData.get('rangeInput');
-                    Renderer.questionnaire.storeResponse(rangeData);
-                    break;
-                }
-            }
-            Renderer.questionnaire.nextIndicator();
-            this.updateProgressBar();
-            this.manager.currentIndicator?.render();
+                    const firstChild = form.querySelector('input') as HTMLInputElement;
+                    switch (firstChild.name) {
+                        case 'multiCheckbox' : {
+                            const checkboxData = formData.get('multiCheckbox');
+                            let booleanData = checkboxData === 'yes' ? true : checkboxData === 'no' ? false : null;
+                            Renderer.questionnaire.storeResponse(booleanData);
+                            break;
+                        }
+                        case 'singleCheckbox': {
+                            const checkboxData = formData.get('singleCheckbox');
+                            let booleanData = checkboxData === 'yes' ? true : checkboxData === 'no' ? false : null;
+                            Renderer.questionnaire.storeResponse(booleanData);
+                            break;
+                        }
+                        case 'rangeInput': {
+                            const rangeData = formData.get('rangeInput');
+                            Renderer.questionnaire.storeResponse(rangeData);
+                            break;
+                        }
+                    }
+                    Renderer.questionnaire.nextIndicator();
+                    this.updateProgressBar();
+                    this.manager.currentIndicator?.render();
+                });
         }
 
         const previousButton = document.getElementById('previousButton');
         if (!previousButton) throw new Error('Previous button not found');
 
         const handlePreviousClick = () => {
-            Renderer.questionnaire.previousIndicator();
-            this.manager.currentIndicator?.render();
-            this.updateProgressBar();
-            console.log('Previous button clicked')
+            Renderer.playAnimationOnElementWithId('indicatorContainer', 'fade-out')
+                .then(() => {
+                    Renderer.questionnaire.previousIndicator();
+                    this.manager.currentIndicator?.render();
+                    this.updateProgressBar();
+                    console.log('Previous button clicked')
+                });
         }
 
-        form.addEventListener('submit', (event: Event) => {
-            event.preventDefault();
-            Renderer.playAnimationOnElementWithId('indicatorContainer', 'fade-out')
-                .then(() => {
-                    handleNextSubmit(event);
-                });
-        });
+        form.removeEventListener('submit', handleNextSubmit);
+        form.addEventListener('submit', handleNextSubmit);
 
-        previousButton.addEventListener('click', () => {
-            Renderer.playAnimationOnElementWithId('indicatorContainer', 'fade-out')
-                .then(() => {
-                    handlePreviousClick();
-                });
-        });
+        previousButton.removeEventListener('click', handlePreviousClick);
+        previousButton.addEventListener('click', handlePreviousClick);
     }
 
     render(): void {
@@ -98,6 +96,7 @@ export class SubsectorRenderer extends Renderer {
         Renderer.attachHTMLToElementWithId('indicatorFormContainer', indicatorFormHTML);
         this.manager.currentIndicator?.render();
         this.attachEventListeners();
+        Renderer.playAnimationOnElementWithId('indicatorFormContainer', 'fade-in');
     }
 
     private updateProgressBar(): void {
