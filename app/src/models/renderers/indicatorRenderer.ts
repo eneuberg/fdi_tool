8aa47d88eef33c50f73a24475dfc9a8320dac44f
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2024-05-04 21:53:40 +0200

    add new comment representation

--- app/src/models/renderers/indicatorRenderer.ts
+++ app/src/models/renderers/indicatorRenderer.ts
@@ -11,25 +11,46 @@ export class IndicatorRenderer extends Renderer {
     }
 
     protected attachEventListeners() {
+        const comment = document.querySelector('.comment') as HTMLElement;
+        const commentText = document.querySelector('.commentText') as HTMLElement;
+
+        function positionCommentText(): void {
+            if (comment && commentText) {
+                const commentRect = comment.getBoundingClientRect();
+                commentText.style.top = `${commentRect.bottom}px`;
+            }
+        }
+
+        positionCommentText();
+
+        window.addEventListener('resize', positionCommentText);
+        window.addEventListener('scroll', positionCommentText);
     }
 
     render(): void {
         const commentHtml = this.manager.comment ? `
-            <span class="comment">
-                <img src="${tooltipIcon}" alt="(?)" width="24" height="24">
-                <span class="commentText bg-dark text-white p-3 text-center">${this.manager.comment}</span>
+            <span class="comment">  
+                <span class="seeMore align-content-center fw-lighter">See more: </span>
+                <img class="align-sub" src="${tooltipIcon}" alt="(?)" width="22" height="22">
+                <span class="commentText bg-dark text-white fw-lighter fs-6 p-3 text-center">${this.manager.comment}</span>
             </span>
-            ` : '';
+                ` : '';
 
         const indicatorHTML = `
             <div class="indicator flex-column align-items-center" id="indicator">
-                <p id="question" class="text-start fw-bold ps-3 mt-3">${this.manager.text}   ${commentHtml}</p>
+                <p id="question" class="text-start fw-bold ps-3 mt-3">
+                    <span class="me-3">
+                        ${this.manager.text} 
+                    </span>
+                ${commentHtml}
+                </p>
                 <div id="evaluationContainer"></div>
             </div>
-            `;
+        `;
 
         Renderer.attachHTMLToElementWithId('indicatorContainer', indicatorHTML);
         this.manager.evaluation.render();
         Renderer.playAnimationOnElementWithId('indicatorContainer', 'fade-in');
+        this.attachEventListeners();
     }
 }
